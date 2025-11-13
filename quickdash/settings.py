54499a937d76bdc0216fd42d7f2bcee3eb47f152
Mdhelaluddin <Mdhelaluddin3391@gmail.com>
2025-11-14 00:18:53 +0530

    added front add desine

--- quickdash/settings.py
+++ quickdash/settings.py
@@ -100,7 +100,7 @@ ROOT_URLCONF = 'quickdash.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -166,6 +166,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/ 'web/static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
